41c50a02bb14af8ef4ca29698bd82d156695e70b
Iskra B <ibatistapoblete@gmail.com>
2019-04-26 04:41:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  new
-  array.collect {|name| puts "Hello, my name is #{name}"}
+  new_array = []
+  array.each do |name| 
+    new_array << "Hello, my name is #{name}"
+  end
+  new_array
 end
 
 def assign_rooms(array)
